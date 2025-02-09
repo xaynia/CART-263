b79789cf6f2748feead74d894f65112fc9670c3e
xaynia <158102890+xaynia@users.noreply.github.com>
2025-02-08 22:48:28 -0500

    Task 3 updated

--- Tasks/CART263-TASK-3/task-3-js/team-b.js
+++ Tasks/CART263-TASK-3/task-3-js/team-b.js
@@ -69,9 +69,9 @@ function setup_B() {
           }
         }
       }
-    } //for
+    }
   }
-   /**************** ANI A ************************************ */
+  /**************** ANI A ************************************ */
   /** PUT ALL YOUR CODE FOR ANIMATION A INSIDE  HERE */
   /**************** ANI A ************************************ */
   /**************** TASK *******************************************
@@ -116,46 +116,76 @@ function setup_B() {
   /**************** ANI C ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE  HERE */
   /**************** ANI C ************************************ */
- /**************** TASK *******************************************
-   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
-   * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
-   * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
-   * do not use  requestAnimationFrame(), setInterval() nor setTimeout() -> meaning keep it simple ;)
-   * 
-   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
-   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
-   * this is so that your styles are not overriden by other teams.
-   * NOTE::: All your code is to be added here inside this function -
-   * remember you can define other functions inside....
-   * Do not change any code above or the HTML markup.
-   * **/
+  /**************** TASK *******************************************
+    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+    * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
+    * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
+    * do not use  requestAnimationFrame(), setInterval() nor setTimeout() -> meaning keep it simple ;)
+    * 
+    * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+    * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+    * this is so that your styles are not overriden by other teams.
+    * NOTE::: All your code is to be added here inside this function -
+    * remember you can define other functions inside....
+    * Do not change any code above or the HTML markup.
+    * **/
 
 
   function aniC(parentCanvas) {
+    // 1. create new <div> -> called divElement
+    let divElement = document.createElement("div");
+    // 2. style div
+    divElement.style.width = "375px";
+    divElement.style.height = "375px";
+    divElement.style.backgroundColor = "black";
+    // so we can see multiple squares side-by-side
+    divElement.style.display = "flex";
+    divElement.style.flexWrap = "wrap";
+
+    // 3. append to parent 
+    parentCanvas.appendChild(divElement);
 
     console.log("in C");
+
     /*** THIS IS THE CALLBACK FOR KEY DOWN ( DO NOT CHANGE THE NAME..) */
     windowKeyDownRef = function (e) {
-      //code for key down in here
-      console.log(e)
-      //SAMPLE KEY CHECK (you do not have to use)
-      if (e.code === "Space") {
-        console.log("team-space down")
+      console.log("Key down:", e.code);
+
+      // 4. change the big square color when key is pressed
+      divElement.style.backgroundColor = "blue";
+
+      // 5. Create smaller squares inside the main div as a pattern
+      for (let i = 0; i < 5; i++) {
+        let newSquare = document.createElement("div");
+        newSquare.style.width = "20px";
+        newSquare.style.height = "20px";
+        newSquare.style.margin = "2px";
+        newSquare.style.backgroundColor = getRandomColor();
+
+        newSquare.style.display = "inline-block";
+
+        divElement.appendChild(newSquare);
       }
     };
 
     /*** THIS IS THE CALLBACK FOR KEY UP ( DO NOT CHANGE THE NAME..) */
     windowKeyUpRef = function (e) {
-    //SAMPLE KEY CHECK (you do not have to use)
-      if (e.code === "Space") {
-        console.log("space up");
-        console.log("team-space up")
-      }
+      console.log("Key up:", e.code);
 
+      // 6. revert color (or do something else) on key release
+      divElement.style.backgroundColor = "black";
     };
 
     //DO NOT REMOVE
     window.addEventListener("keydown", windowKeyDownRef);
     window.addEventListener("keyup", windowKeyUpRef);
+
+    // Helper function to pick a random color
+    function getRandomColor() {
+      // array of colors
+      const colors = ["red", "blue", "green", "orange", "purple", "yellow", "pink"];
+      let randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    }
   }
 }
